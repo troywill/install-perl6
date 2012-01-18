31744c7d5d6c4dd1247ae5c157c695b9192bba42
Troy Will <troydwill@gmail.com>
2012-01-17 22:29:11 -0800

    05-stow-rakudo-star.sh

--- 01_step_by_step/05-stow-rakudo-star.sh
+++ 01_step_by_step/05-stow-rakudo-star.sh
@@ -1,2 +1,8 @@
-  201  perl Configure.pl --prefix=/usr/local --bindir=/usr/local/bin/perl6
-  202  perl Configure.pl --prefix=/usr/local
+#!/bin/sh
+VERSION='2011.07'
+perl Configure.pl --prefix=/usr/local
+sudo make DESTDIR=/stow/perl6-${VERSION} install
+sudo stow2 perl6-${VERSION}
+
+
+
