93a6fbdb0722a7b16adbd45f6b5fa1ba87b2ab91
Troy Will <troydwill@gmail.com>
2012-01-25 08:05:42 -0800

    04-stow-parrot.sh

--- 01_step_by_step/04-stow-parrot.sh
+++ 01_step_by_step/04-stow-parrot.sh
@@ -1,2 +1,13 @@
-perl Configure.pl --prefix=/usr/local --bindir=/usr/local/bin/perl6 && make && \
-    sudo make DESTDIR=/stow/parrot-3.6.0 install && sudo stow2 parrot-3.6.0
+#!/bin/bash
+set -o errexit
+set -o verbose
+set -o nounset
+
+PARROT_VERSION=3.6.0
+PREFIX=/usr/local
+BINDIR=$PREFIX/perl6
+
+perl Configure.pl --prefix=$PREFIX --bindir=$BINDIR
+make
+sudo make DESTDIR=/stow/parrot-$PARROT_VERSION install
+sudo stow parrot-$PARROT_VERSION
