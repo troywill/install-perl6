887c2867cf3ece80561f52b5dd8ec5dcd34ec614
Troy Will <troydwill@gmail.com>
2012-02-02 12:48:58 -0800

    05-stow-nqp.sh

---
+++ 01_step_by_step/05-stow-nqp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -o errexit
+set -o verbose
+set -o nounset
+
+PACKAGE='nqp'
+VERSION='2012.01'
+PREFIX='/usr/local'
+DESTDIR="/stow/nqp-${VERSION}"
+
+# bindir is not known 2012-02-02
+perl Configure.pl --prefix=$PREFIX
+make
+sudo make DESTDIR=$DESTDIR install
+sudo stow ${PACKAGE}-${VERSION}
