ada4baf59fc2a7a95d92ac59dda0a9feba82073f
Troy Will <troydwill@gmail.com>
2012-02-02 12:27:44 -0800

    05-stow-rakudo-star.sh: update to version 2012-01

--- 01_step_by_step/05-stow-rakudo-star.sh
+++ 01_step_by_step/05-stow-rakudo-star.sh
@@ -1,8 +1,5 @@
-#!/bin/sh
-VERSION='2011.07'
+#!/bin/bash
+VERSION='2012.01'
 perl Configure.pl --prefix=/usr/local
 sudo make DESTDIR=/stow/perl6-${VERSION} install
 sudo stow perl6-${VERSION}
-
-
-
