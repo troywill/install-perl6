a6279631a13deb0bc86a80795b2f1bbc3303467f
Troy Will <troydwill@gmail.com>
2012-02-02 13:39:09 -0800

    06-stow-rakudo-star.sh

--- 01_step_by_step/06-stow-rakudo-star.sh
+++ 01_step_by_step/06-stow-rakudo-star.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+set -o errexit
+PACKAGE='perl6'
 VERSION='2012.01'
+DESTDIR="/stow/${PACKAGE}-${VERSION}"
 perl Configure.pl --prefix=/usr/local
-sudo make DESTDIR=/stow/perl6-${VERSION} install
-sudo stow perl6-${VERSION}
+sudo make DESTDIR=${DESTIDR} install
+sudo stow ${PACKAGE}-${VERSION}
